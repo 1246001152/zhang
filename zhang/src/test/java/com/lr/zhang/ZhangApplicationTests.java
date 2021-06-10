655ec6c603970219aa4e93f7f6abc2e9f6491b64
zhangjinfei <Apple5201314>
2021-06-10 08:38:15 +0800

    导出test

--- zhang/src/test/java/com/lr/zhang/ZhangApplicationTests.java
+++ zhang/src/test/java/com/lr/zhang/ZhangApplicationTests.java
@@ -3,12 +3,27 @@ package com.lr.zhang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @SpringBootTest
 class ZhangApplicationTests {
 
     @Test
     void contextLoads() {
         System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        Map<String,Object> map = new LinkedHashMap<>();
+        map.put("abc","a1");
+        map.put("book","a2");
+        map.put("city","a3");
+        map.put("dis","a4");
+
+        System.out.println(map);
+
+
     }
 
 }
